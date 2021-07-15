d06ae60ff1c8441147aa171fb9976e437122b22d
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2021-07-15 23:27:24 +0900

    display tokyo

--- components/map.tsx
+++ components/map.tsx
@@ -8,6 +8,25 @@ const LNG = 139.76778;
 const ZOOM = 15;
 
 const Map = () => {
+  const display = [
+    "東日本旅客鉄道",
+    "東京地下鉄",
+    "西武鉄道",
+    "東武鉄道",
+    "東急電鉄",
+    "東京都",
+    "京王電鉄",
+    "京成電鉄",
+    "京浜急行電鉄",
+    "小田急電鉄",
+    "多摩都市モノレール",
+    "東京臨海高速鉄道",
+    "北総鉄道",
+    "東京モノレール",
+    "ゆりかもめ",
+    "埼玉高速鉄道",
+    "首都圏新都市鉄道",
+  ];
   return (
     <div>
       <MapContainer
@@ -19,8 +38,17 @@ const Map = () => {
           attribution="<a href='https://maps.gsi.go.jp/development/ichiran.html' target='_blank'>地理院タイル</a>"
           url="https://cyberjapandata.gsi.go.jp/xyz/pale/{z}/{x}/{y}.png"
         />
-        <GeoJSON data={Railroad} />
-        <GeoJSON data={Station} style={{ color: "red" }} />
+        <GeoJSON
+          data={Railroad.features.filter((r: any) =>
+            display.includes(r.properties.N02_004)
+          )}
+        />
+        <GeoJSON
+          data={Station.features.filter((r: any) =>
+            display.includes(r.properties.N02_004)
+          )}
+          style={{ color: "red" }}
+        />
       </MapContainer>
     </div>
   );
