b736492b5ba3a35b9c70852c7b842b2b3806bd24
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2021-07-15 22:42:58 +0900

    add GeoJSON

--- components/map.tsx
+++ components/map.tsx
@@ -1,9 +1,11 @@
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer, GeoJSON } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import Railroad from "../data/N02-20_RailroadSection.geojson";
+import Station from "../data/N02-20_Station.geojson";
 
-const LAT = 35.3622222;
-const LNG = 138.7313889;
-const ZOOM = 5;
+const LAT = 35.68294;
+const LNG = 139.76778;
+const ZOOM = 15;
 
 const Map = () => {
   return (
@@ -15,8 +17,10 @@ const Map = () => {
       >
         <TileLayer
           attribution="<a href='https://maps.gsi.go.jp/development/ichiran.html' target='_blank'>地理院タイル</a>"
-          url="https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png"
+          url="https://cyberjapandata.gsi.go.jp/xyz/pale/{z}/{x}/{y}.png"
         />
+        <GeoJSON data={Railroad} />
+        <GeoJSON data={Station} style={{ color: "red" }} />
       </MapContainer>
     </div>
   );
--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   reactStrictMode: true,
-}
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.geojson$/,
+      use: ["json-loader"],
+    });
+    return config;
+  },
+};
