63815bb629edf86ee14b503b5d6880270b6cd72e
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2021-08-09 15:09:22 +0900

    update style

--- components/map.tsx
+++ components/map.tsx
@@ -7,29 +7,36 @@ import {
   useMapEvents,
 } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-// @ts-ignore
-import Railroad from "@/data/N02-20_RailroadSection.geojson";
-// @ts-ignore
-import Station from "@/data/N02-20_Station.geojson";
 
 const LAT = 35.68294;
 const LNG = 139.76778;
-const ZOOM = 15;
+const ZOOM = 14;
 
-function SetCenter({ center }: { center: LatLngExpression }) {
+function SetCenter({
+  center,
+  zoom,
+}: {
+  center: LatLngExpression;
+  zoom: number;
+}) {
   const map = useMap();
-  map.setView(center, ZOOM);
+  map.setView(center, zoom);
   return null;
 }
 
 function DragEvent({
   setLatLng,
+  setZoom,
 }: {
   setLatLng: (center: LatLngLiteral) => void;
+  setZoom: (level: number) => void;
 }) {
   useMapEvents({
     dragend: (e: any) => {
       setLatLng(e.target.getCenter());
+    },
+    zoomend: (e: any) => {
+      setZoom(e.target.getZoom());
     },
   });
   return null;
@@ -38,49 +45,23 @@ function DragEvent({
 const Map = ({
   format = "std",
   latlng = [LAT, LNG],
+  zoom = ZOOM,
   setLatLng = () => {},
+  setZoom = () => {},
 }: {
-  format: String;
+  format: string;
   latlng: LatLngExpression;
+  zoom: number;
   setLatLng: (center: LatLngLiteral) => void;
+  setZoom: (level: number) => void;
 }) => {
-  const display = [
-    "東日本旅客鉄道",
-    "東京地下鉄",
-    "西武鉄道",
-    "東武鉄道",
-    "東急電鉄",
-    "東京都",
-    "京王電鉄",
-    "京成電鉄",
-    "京浜急行電鉄",
-    "小田急電鉄",
-    "多摩都市モノレール",
-    "東京臨海高速鉄道",
-    "北総鉄道",
-    "東京モノレール",
-    "ゆりかもめ",
-    "埼玉高速鉄道",
-    "首都圏新都市鉄道",
-  ];
   return (
-    <MapContainer style={{ height: "300px", width: "400px" }}>
-      <SetCenter center={latlng} />
-      <DragEvent setLatLng={setLatLng} />
+    <MapContainer style={{ height: "45vh", width: "45vw" }}>
+      <SetCenter center={latlng} zoom={zoom} />
+      <DragEvent setLatLng={setLatLng} setZoom={setZoom} />
       <TileLayer
         attribution="<a href='https://maps.gsi.go.jp/development/ichiran.html' target='_blank'>地理院タイル</a>"
         url={`https://cyberjapandata.gsi.go.jp/xyz/${format}/{z}/{x}/{y}.png`}
-      />
-      <GeoJSON
-        data={Railroad.features.filter((r: any) =>
-          display.includes(r.properties.N02_004)
-        )}
-      />
-      <GeoJSON
-        data={Station.features.filter((r: any) =>
-          display.includes(r.properties.N02_004)
-        )}
-        style={{ color: "red" }}
       />
     </MapContainer>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,14 @@ import dynamic from "next/dynamic";
 import { useMemo, useState } from "react";
 import { LatLngLiteral } from "leaflet";
 
+import styles from "@/styles/Home.module.css";
+
 const Home = () => {
   const loading = () => <p>A map is loading</p>;
 
   const [latitude, setLatitude] = useState(35.68294);
   const [longitude, setLongitude] = useState(139.76778);
+  const [zoom, setZoom] = useState(14);
 
   const setLatLng = (center: LatLngLiteral) => {
     setLatitude(center.lat);
@@ -22,22 +25,57 @@ const Home = () => {
     []
   );
   return (
-    <main>
-      <label>latitude</label>
-      <input
-        type="number"
-        value={latitude}
-        onChange={(e) => setLatitude(Number(e.target.value))}
-      />
-      <label>longitude</label>
-      <input
-        type="number"
-        value={longitude}
-        onChange={(e) => setLongitude(Number(e.target.value))}
-      />
-      <button>描画</button>
-      <Map format="std" latlng={[latitude, longitude]} setLatLng={setLatLng} />
-      <Map format="pale" latlng={[latitude, longitude]} setLatLng={setLatLng} />
+    <main className={styles.main}>
+      <header>
+        <label>latitude</label>
+        <input
+          type="number"
+          value={latitude}
+          onChange={(e) => setLatitude(Number(e.target.value))}
+        />
+        <label>longitude</label>
+        <input
+          type="number"
+          value={longitude}
+          onChange={(e) => setLongitude(Number(e.target.value))}
+        />
+        <label>zoom</label>
+        <input
+          type="number"
+          value={zoom}
+          onChange={(e) => setZoom(Number(e.target.value))}
+        />
+      </header>
+      <section className={styles.section}>
+        <Map
+          format="std"
+          latlng={[latitude, longitude]}
+          zoom={zoom}
+          setLatLng={setLatLng}
+          setZoom={setZoom}
+        />
+        <Map
+          format="lum200k"
+          latlng={[latitude, longitude]}
+          zoom={zoom}
+          setLatLng={setLatLng}
+          setZoom={setZoom}
+        />
+        <Map
+          format="lcm25k"
+          latlng={[latitude, longitude]}
+          zoom={zoom}
+          setLatLng={setLatLng}
+          setZoom={setZoom}
+        />
+        <Map
+          format="relief"
+          latlng={[latitude, longitude]}
+          zoom={zoom}
+          setLatLng={setLatLng}
+          setZoom={setZoom}
+        />
+      </section>
     </main>
   );
 };
